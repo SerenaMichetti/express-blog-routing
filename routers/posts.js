911721ad607665072f167f05ee73f528bac9c51e
Serena <sere.michetti@icloud.com>
2026-02-23 20:58:09 +0100

    importato nel file di router posts l'array di post e nella chiamata index la richiamo in formato json

--- routers/posts.js
+++ routers/posts.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router();
-
+const posts = require ('../data/posts')
 
 // index
 router.get('/', (req, res) => {
     res.send('Hai richiesto tutti i posts');
+    res.json(posts);
 })
 
 // show
