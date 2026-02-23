92027ffc6e52bffe455c685973874ec5bd56d02c
Serena <sere.michetti@icloud.com>
2026-02-23 20:12:23 +0100

    Aggiunto router dei posts con le operazioni CRUD

--- api.js
+++ api.js
@@ -1,1 +1,23 @@
 console.log("Welcome blog world")
+
+const express = require('express')
+
+//importo il router posts
+const postsRouter = require("./routers/posts")
+
+const app = express()
+const port = 3000
+
+app.use(express.static('public'));
+
+//rotta base con hello world
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+//richiamo la variabile posts router e inserisco il prefisso /posts
+app.use("/posts", postsRouter)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
---
+++ routers/posts.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const router = express.Router();
+
+
+// index
+router.get('/', (req, res) => {
+    res.send('Hai richiesto tutti i posts');
+})
+
+// show
+router.get('/:id', function (req, res) {
+    res.send(`Hai richiesto di visualizzare il post con id: ${req.params.id}`);
+});
+// store
+router.post('/', function (req, res) {
+    res.send('Hai creato un nuovo post');
+});
+// update
+router.put('/:id', function (req, res) {
+    res.send(`hai richiesto di modificare completamente il post con id: ${req.params.id}`);
+});
+// modify
+router.patch('/:id', function (req, res) {
+    res.send(`Hai richiesto di modificare parzialmente il post con id: ${req.params.id}`);
+});
+// destroy
+router.delete('/:id', function (req, res) {
+    res.send(`Hai richiesto l'eliminazione del post con id: ${req.params.id}`);
+});
+
+
+
+
+module.exports = router;
